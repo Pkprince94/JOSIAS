2b0d17893ffddc017221bb4d2909272fdbfa4e36
prince kisunzu <princekisunzu9@gmail.com>
2025-07-08 19:27:44 +0200

    bulder

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 console.log('vite.config.js loaded')
 
@@ -9,4 +10,13 @@ export default defineConfig({
     port: 5154,
     strictPort: true
   },
-});
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  base: '/', // nécessaire pour bon fonctionnement sur Render
+})
