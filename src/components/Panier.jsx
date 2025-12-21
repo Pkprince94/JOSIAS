1e05e75a23de436559d866d8d33d99a4b71ae7ac
prince kisunzu <princekisunzu9@gmail.com>
2025-12-21 23:13:14 +0100

    p

--- src/components/Panier.jsx
+++ src/components/Panier.jsx
@@ -101,6 +101,16 @@ const Panier = () => {
 
             <div className="d-flex justify-content-end align-items-center gap-3 mt-3">
               <h5>Total: {getTotalPrice().toFixed(0)} $</h5>
+              <form action="https://marchand.maishapay.online/payment/vers1.0/merchant/checkout" method="POST">
+              <input type="hidden" name="gatewayMode" value="0">  
+              <input type="hidden" name="publicApiKey" value="MP-LIVEPK-FUaorbsYg9LQyPuVWq3/2yelYopZz$3bu2weHuBryyqyPsfN04TvgMr/fNbG7422734Go0pl3cEer$aiQjtDvRs$T4$8u60uwoHX.11OBtc5.jFIF$sdeQ$M">
+              <input type="hidden" name="secretApiKey" value="MP-LIVESK-bVjCT74T18Ef0QK3$l$TXS1RM4.fuX0JoB9FZehy$aZwGLe3qI.$1NSh61z1.5P/WM$a$sqiKLn1Y.yTm3CxXaXnoX1g7rEHQWAg8d2afFu2ZboIRTbs4FTy">
+              <input type="hidden" name="montant" value={getTotalPrice().toFixed(0)}>
+              <input type="hidden" name="devise" value="USD">
+              <input type="hidden" name="callbackUrl" value="">
+              <input type="submit" value="Payer">
+            </form>
+            
               <button className="btn btn-success" onClick={handleCheckout}>
                 Passer au paiement
               </button>
